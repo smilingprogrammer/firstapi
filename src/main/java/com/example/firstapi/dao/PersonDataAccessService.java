865340c561a3aa50ed9056edda34961767c00a1c
Oyewale Abdulsobur <66224527+smilingprogrammer@users.noreply.github.com>
2022-12-22 03:01:35 +0100

    selectAllPeople from database successfully working

--- src/main/java/com/example/firstapi/dao/PersonDataAccessService.java
+++ src/main/java/com/example/firstapi/dao/PersonDataAccessService.java
@@ -1,14 +1,21 @@
 package com.example.firstapi.dao;
 
 import com.example.firstapi.model.Person;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository("postgres")
 public class PersonDataAccessService implements PersonDao{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertPerson(UUID id, Person person) {
         return 0;
@@ -16,12 +23,26 @@ public class PersonDataAccessService implements PersonDao{
 
     @Override
     public List<Person> selectAllPeople() {
-        return List.of(new Person(UUID.randomUUID(), "name FROM POSTGRES Database"));
+        final String sql = "SELECT id,name FROM person";
+        return jdbcTemplate.query(sql, (resultSet, i) -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(id, name);
+        });
     }
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        return Optional.empty();
+        final String sql = "SELECT id,name FROM person";
+
+        Person person = jdbcTemplate.queryForObject(sql,
+                new Object[]{id},
+                (resultSet, i) -> {
+            UUID personId = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(personId, name);
+        });
+        return Optional.ofNullable(person);
     }
 
     @Override
